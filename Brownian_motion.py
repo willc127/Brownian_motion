28c2cd1675697862fdd0c970d210c0467a2c82e1
William Cesar <williamcesar.ribeiro@outlook.com>
2023-01-21 19:45:55 -0300

    Calculated the distance

--- Brownian_motion.py
+++ Brownian_motion.py
@@ -1,19 +1,23 @@
 import turtle as t
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import random
+import csv
 
 # create a brownian motion function
+
+
 def brownian(scale, steps, dist):
-    #Specify screen properties
+    # Specify screen properties
     scr = t.Screen()
     scr.setup(width=1.0, height=1.0)
     (width, height) = scr.screensize()
 
     # Specify background color
     t.bgcolor('black')
-    
-    #Specify turtle properties
+
+    # Specify turtle properties
     t.shape('circle')
     t.shapesize(stretch_wid=0.75, stretch_len=0.75)
     t.width(2)
@@ -21,29 +25,76 @@ def brownian(scale, steps, dist):
     # Specify speed of drawing; 0 = fastest
     t.speed(0)
 
-    #Get a color from a colormap for each step
+    # Get a color from a colormap for each step
     row = np.linspace(0.0, 1.0, steps)
-    rgb = mpl.colormaps['jet'](row)[np.newaxis, :, :3] #Convert color to rgb
+    rgb = mpl.colormaps['jet'](row)[np.newaxis, :, :3]  # Convert color to rgb
 
-    #Draw the random walk of a particle
-    for i in range(0,steps):
-        #Get a color for the particle
+    x = np.zeros(steps)
+    y = np.zeros(steps)
+
+    # Draw the random walk of a particle
+    for i in range(0, steps):
+        # Get a color for the particle
         t.pencolor(rgb[0][i])
-        t.color(rgb[0][i]) 
-        if dist =='normal':
-            t.forward(random.gauss()*scale) #Move forward with a gaussian distribution
-            t.right(random.gauss()*360) #Turn with a gaussian distribution
-            (x,y) =t.pos() #get position in each step
-            if abs(x) > width or abs(y) > height: #If particle is out of screen, turn back
+        t.color(rgb[0][i])
+        if dist == 'normal':
+            # Move forward with a gaussian distribution
+            (x[i], y[i]) = t.pos()  # get position in each step
+            t.forward(random.gauss(0, 1)*scale)
+            # Turn with a gaussian distribution
+            t.right(random.gauss(0, 1)*360)
+            # If particle is out of screen, turn back
+            if abs(x[i]) > width or abs(y[i]) > height:
                 t.right(180)
         elif dist == 'random':
-            t.forward(random.random()*scale) #Move forward with a random distribution
-            t.right(random.random()*360) #Turn with a random distribution
-            (x,y) =t.pos() #get position in each step
-            if abs(x) > width or abs(y) > height: #If particle is out of screen, turn back
+            # Move forward with a random distribution
+            (x[i], y[i]) = t.pos()  # get position in each step
+            t.forward(random.random()*scale)
+            t.right(random.random()*360)  # Turn with a random distribution
+            # If particle is out of screen, turn back
+            if abs(x[i]) > width or abs(y[i]) > height:
                 t.right(180)
+    return [x, y]
     t.done()
 
-#Call function
 
-brownian(20, 1000, 'random')
+# Call function
+steps = 50
+scale = 15
+dist_type = 'normal'
+# Set random seed for reproducibility random[0,3,4,6,8] normal[3,5,6,9,42
+random.seed(9)
+
+#Get positions of brownian motion
+(X, Y) = brownian(scale, steps, dist_type)
+
+# Calculate the total distance of particle
+r = np.zeros(steps)
+time = np.arange(steps)
+
+#Open file to write the results
+with open('results.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Time', 'X', 'Y', 'r'])
+    for i in range(steps):
+        r[i] = (X[i]**2 + Y[i]**2)**0.5 #Calculate the total distance of particle
+        writer.writerow([time[i],X[i], Y[i], r[i]])
+
+#Plot the results
+fig = plt.figure(0)
+plt.plot(time, r, 'bo', linewidth=2)
+plt.xlabel('Time')
+plt.ylabel('Distance')
+plt.show()
+
+fig1 = plt.figure(1)
+plt.plot(time, X, 'bo', linewidth=2)
+plt.xlabel('Time')
+plt.ylabel('Distance in X')
+plt.show()
+
+fig2 = plt.figure(2)
+plt.plot(time, Y, 'bo', linewidth=2)
+plt.xlabel('Time')
+plt.ylabel('Distance in Y')
+plt.show()
